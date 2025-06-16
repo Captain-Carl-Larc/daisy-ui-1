a0b2c044fb2bace1c562069e0872866e70f6c52c
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-06-16 22:04:15 +0300

    heading done

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -2,20 +2,23 @@
 import './../app.css';
 
 function Homepage() {
+    const hero = (<div data-theme = "dark"  className="hero min-h-screen"
+  style={{ backgroundColor: '#1a202c' }}
+>
+  <div className="hero-overlay"></div>
+  <div  className="hero-content margin-0 text-neutral-content text-center">
+    <div className="">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">Unlock <span className='text-blue-700'>Million-Dollar</span> Trade Opportunities Across 4 Continents</h1>
+      <p className="mb-5">
+        Master the art of importing from China, Turkey, UK, Dubai & USA while exporting Africa's agricultural gold to hungry global markets
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>)
     return ( 
         <>
-            <div className="small-">
-
-            </div>
-            <div className="heading">
-            
-            </div>
-            <div className="sub-topic">
-
-            </div>
-            <div className="cta-containers">
-
-            </div>
+        {hero}
         </>
      );
 }
